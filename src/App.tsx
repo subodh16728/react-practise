139b960a39280aadc0b073925db5193d37e0e9e2
Subodh Yadav <subodh16728@gmail.com>
2026-01-10 17:30:20 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Hooks from "./components/Hooks";
+
+function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <Hooks />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hooks.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+
+const Hooks = () => {
+  const [count, setCount] = useState(0);
+  const [numberOfTimesClicked, setNumberOfTimesClicked] = useState(0);
+
+  // without dependency array - useEffect will run on every re-render
+  useEffect(() => {
+    console.log("Hello World!");
+  });
+
+  // empty dependency array - useEffect will run only once
+  useEffect(() => {
+    console.log("Hello World!");
+  }, []);
+
+  // dependency array with something - useEffect will run whenever the value of the variable mentioned in the dependency array changes
+  useEffect(() => {
+    console.log("Count clicked");
+  }, [count]);
+
+  useEffect(() => {
+    console.log("numberOfTimesClicked clicked");
+  }, [numberOfTimesClicked, count]);
+
+  return (
+    <>
+      <div>UseEffect hook</div>
+      <p>Count is: {count}</p>
+
+      {/* Count */}
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
+
+      {/* NumberOfTimesClicked */}
+      <p>Number of times clicked is: {numberOfTimesClicked}</p>
+      <button
+        onClick={() =>
+          setNumberOfTimesClicked((prevClickCount) => prevClickCount + 1)
+        }
+      >
+        Increase count
+      </button>
+      <button
+        onClick={() =>
+          setNumberOfTimesClicked((prevClickCount) => prevClickCount - 1)
+        }
+      >
+        Decrease count
+      </button>
+
+      {console.log("Render complete")}
+    </>
+  );
+};
+
+export default Hooks;
