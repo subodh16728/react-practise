a5a92d9d686ae68eae87da1e673e27a76e7252b5
Subodh Yadav <subodh16728@gmail.com>
2026-01-18 17:57:38 +0530

    added comments for better understanding

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Dashboard from "./components/Dashboard";
 import Header from "./components/Header";
 import Hooks from "./components/Hooks";
 
-const ProtectedDashboard = withAuth(Dashboard);
+const ProtectedDashboard = withAuth(Dashboard); // till here - the unexecuted component is returned. Nothing inside it has been executed. It will be executed when this component is rendered. That is below
 
 function App() {
   return (
--- src/auth/withAuth.tsx
+++ src/auth/withAuth.tsx
@@ -1,15 +1,16 @@
 // this is a HOC
 
 const withAuth = (WrappedComponent: any) => {
+  // returns the whole component (basic functionality of HOC's)
   return (props: any) => {
     const isLoggedIn = true;
 
     if (!isLoggedIn) {
-      return <h2>Please login first</h2>;
+      return <h2>Please login first</h2>; // returns if this should be rendered based on condition
     }
 
     // IMPORTANT LINE 👇
-    return <WrappedComponent {...props} />;
+    return <WrappedComponent {...props} />; // returns if this should be rendered based on condition
   };
 };
 
